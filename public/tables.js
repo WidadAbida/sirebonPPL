4b314b1c64b30dc186981acf992f160090f5aa10
unknown <aryasaktiwirasena@gmail.com>
2020-12-01 10:41:38 +0700

    Tabel request sudah dinamis, tabel daftar pasien sekarang bisa render

--- public/tables.js
+++ public/tables.js
@@ -1,9 +1,85 @@
 // TODO :   Buat display tabel-tabel di daftarpasien, jadwal, requestresep
 //          Munculin badge notif (berarti listener firestorenya disini ya)
 
-
 // tabel jadwal
+function renderJadwal(doc) {
+    if(doc.exists){
+        for(i=1;i<doc.data().obat.length;i++){
+            let tr = document.createElement('tr')
+            let nama = document.createElement('td')
+            let minum = document.createElement('td')
+            let sebses = document.createElement('td')
+            let quantity = document.createElement('td')
 
+            nama.innerText = doc.data().obat[i]
+            minum.innerText = doc.data().frekuensi[i]
+            sebses.innerText = doc.data().sebses[i]
+            quantity.innerText = doc.data().quantity[i]
+            
+            tr.appendChild(nama)
+            tr.appendChild(minum)
+            tr.appendChild(sebses)
+            tr.appendChild(quantity)
+
+            jadwal.appendChild(tr)
+        }
+    }
+}
+
+//tabel requestResep
+function renderRequestResep(doc) {
+    if(doc.exists){
+        for(i=0;i<doc.data().pasien.length;i++){
+            let tr = document.createElement('tr')
+            let pasien = document.createElement('td')
+            let penyakit = document.createElement('td')
+            let obat = document.createElement('td')
+            let botantd = document.createElement('td')
+            let botan = document.createElement('button')
+
+
+            botan.className = 'delButton'
+            botan.setAttribute('onclick', 'delRow(this)')
+            botan.innerText = 'Done'
+
+            pasien.innerText = doc.data().pasien[i]
+            penyakit.innerText = doc.data().penyakit[i]
+            obat.innerText = doc.data().obat[i]
+            botantd.appendChild(botan)
+
+            tr.appendChild(pasien)
+            tr.appendChild(penyakit)
+            tr.appendChild(obat)
+            tr.appendChild(botantd)
+
+            requestResep.appendChild(tr)
+        }    
+    }
+    else{
+        console.log('Document does not exist')
+    }
+}
+
+//tabel daftarPasien
+function renderDaftarPasien(namaPasien, doc){
+    for(i=1;i<doc.data().obat.length;i++){
+        let tr = document.createElement('tr')
+        let nama = document.createElement('td')
+        let obat = document.createElement('td')
+        let quantity = document.createElement('td')
+
+        nama.innerText = namaPasien
+        obat.innerText = doc.data().obat[i]
+        quantity.innerText = doc.data().quantity[i] 
+        
+        tr.appendChild(nama)
+        tr.appendChild(obat)
+        tr.appendChild(quantity)
+
+        daftarPasien.appendChild(tr)
+    }
+    
+}
 auth.onAuthStateChanged(function(user) {
     if (user) {
         const jadwal = document.querySelector('#jadwal')
@@ -11,74 +87,43 @@ auth.onAuthStateChanged(function(user) {
         if(jadwal){
             const email = user.email
 
-            db.collection('jadwal').doc(email).get().then(function(doc) {
-                if(doc.exists){
-                    for(i=1;i<doc.data().obat.length;i++){
-                        let tr = document.createElement('tr')
-                        let nama = document.createElement('td')
-                        let minum = document.createElement('td')
-                        let sebses = document.createElement('td')
-                        let quantity = document.createElement('td')
-
-                        nama.innerText = doc.data().obat[i]
-                        minum.innerText = doc.data().frekuensi[i]
-                        sebses.innerText = doc.data().sebses[i]
-                        quantity.innerText = doc.data().quantity[i]
-                        
-                        tr.appendChild(nama)
-                        tr.appendChild(minum)
-                        tr.appendChild(sebses)
-                        tr.appendChild(quantity)
-
-                        jadwal.appendChild(tr)
-                    }    
-                }
-                else{
-                    
-                }
+            db.collection('jadwal').doc(email).get().then( (doc) => {
+                renderJadwal(doc)
             })
         }
 
         const requestResep = document.querySelector('#requestResep')
 
         if(requestResep){
-            console.log(user)
             const email = user.email
 
-            db.collection('requestResep').doc(email).get().then(function(doc) {
-                if(doc.exists){
-                    for(i=0;i<doc.data().pasien.length;i++){
-                        let tr = document.createElement('tr')
-                        let pasien = document.createElement('td')
-                        let penyakit = document.createElement('td')
-                        let obat = document.createElement('td')
-                        let botantd = document.createElement('td')
-                        let botan = document.createElement('button')
-
-
-                        botan.className = 'button'
-                        botan.innerText = 'Done'
-
-                        pasien.innerText = doc.data().pasien[i]
-                        penyakit.innerText = doc.data().penyakit[i]
-                        obat.innerText = doc.data().obat[i]
-                        botantd.appendChild(botan)
-
-                        tr.appendChild(pasien)
-                        tr.appendChild(penyakit)
-                        tr.appendChild(obat)
-                        tr.appendChild(botantd)
-
-                        requestResep.appendChild(tr)
-                    }    
-                }
-                else{
-                    console.log('Document does not exist')
-                }
+            db.collection('requestResep').doc(email).get().then((doc) => {
+                renderRequestResep(doc)
             })
         }
-        else{
-            console.log('No requestResep')
+
+        const daftarPasien = document.querySelector('#daftarPasien')
+
+        if(daftarPasien){
+            const email = user.email
+
+            db.collection('dokter').doc(email).get().then(function(doc){
+                const namaDokter = doc.data().nama
+
+                db.collection('jadwal').get().then((snap) => {
+                    snap.forEach(doc => {
+                        const emailPasien = doc.id
+                        console.log(emailPasien)
+
+                        db.collection('pasien').doc(emailPasien).get().then((pasien) => {
+                            console.log(pasien.data().dokter, namaDokter)
+                            if(pasien.data().dokter == namaDokter){
+                                renderDaftarPasien(pasien.data().nama, doc)
+                            }
+                        })
+                    });
+                })
+            })
         }
     } else {
       console.log('No user logged in')
